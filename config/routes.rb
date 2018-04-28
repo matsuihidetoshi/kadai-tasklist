abcdc26e99a2aebe0e4efce09cfbfacbfa055a8a
h.hidetoshi <partyrockeverydayimshufflin@gmail.com>
2018-04-28 07:47:12 +0000

    log in set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+
   root to: 'tasks#index'
+  
+  #sign up
+  get 'signup', to: 'user#new'
+  resources :users, only: [:index, :show, :new, :create]
+  
+  #login
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   resources :tasks
 end
